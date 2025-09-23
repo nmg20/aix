fe7375c5291e80b456e87f0e3925b5f8d89c335d
nmg20 <nicogm977@gmail.com>
2025-09-23 13:07:28 +0200

    adapted (halfway) call

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,13 +2,14 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:8000";
 
-export const parsePlaylist = async (url, page=1, size=20) => {
+export const parsePlaylist = async (url, page=1, size=20, search="") => {
+  params: { 
+    url , page, size
+  }
+  if (search) params.search = search
   const response = await axios.get(
-    `${API_URL}/playlists/parse`, {
-      params: { url 
-        , page, size,
-      }
-    }
+    `${API_URL}/playlists/parse`,
+      { params }
   );
   return response.data;
 };
